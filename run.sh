7a578b3a46e195c6908c9893cb2f1b4f5175e5dc
John Pope <jp@bellgeorge.com>
2024-08-14 17:43:02 +1000

    ok

--- run.sh
+++ run.sh
@@ -1,2 +1,26 @@
-# docker run -it gcr.io/kommunityproject/pytorch-training:v1.04 --env-file ./env.list
-docker run -it -e GCS_BUCKET_NAME="gs://jp-ai-experiments" -e BRANCH_NAME="feat/ada-fixed4" -e GITHUB_REPO="https://github.com/johndpope/imf.git" gcr.io/kommunityproject/pytorch-training:v1.04
+#!/bin/bash
+
+# Set your environment variables
+export GCP_PROJECT=kommunityproject
+export IMAGE_NAME=pytorch-training
+export GCS_BUCKET_NAME="gs://jp-ai-experiments"
+export BRANCH_NAME="feat/ada-fixed4"
+export GITHUB_REPO="https://github.com/johndpope/imf.git"
+
+# Get the latest version
+LATEST_TAG=$(gcloud container images list-tags gcr.io/$GCP_PROJECT/$IMAGE_NAME --format='get(tags)' --sort-by=~tags | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)
+
+if [ -z "$LATEST_TAG" ]; then
+  echo "No valid tags found. Make sure you have pushed images with valid version tags."
+  exit 1
+fi
+
+echo "Latest tag found: $LATEST_TAG"
+
+# Run the Docker container with the latest tag
+echo "Running Docker container with the latest tag: $LATEST_TAG"
+docker run -it \
+  -e GCS_BUCKET_NAME="$GCS_BUCKET_NAME" \
+  -e BRANCH_NAME="$BRANCH_NAME" \
+  -e GITHUB_REPO="$GITHUB_REPO" \
+  gcr.io/$GCP_PROJECT/$IMAGE_NAME:$LATEST_TAG
